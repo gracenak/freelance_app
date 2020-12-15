7586c0f1713487d59720531e32f01d1fd2d0af69
gracenak <gracenak@gmail.com>
2020-12-14 18:30:44 -0700

    add welcome controller

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,2 +1,25 @@
 class GigsController < ApplicationController
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def new
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+        
+    end
+
+
 end
