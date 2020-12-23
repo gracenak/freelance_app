5d31d353656a29ae0d9ea21464b77574971c3dc4
gracenak <gracenak@gmail.com>
2020-12-22 17:06:10 -0700

    add titelize method to ensure all instruments are titelized and no duplicates in DB

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -11,24 +11,21 @@ class Gig < ApplicationRecord
     validates :title, :datetime, :description, presence: true
     validates :payment, numericality: {greater_than_or_equal_to: 0}
 
-
     def instruments_attributes=(instrument_attributes)
-        instrument_attributes.values.each do |instrument_attribute| 
-          if instrument_attribute[:name].present?
-          instrument = Instrument.find_or_create_by(instrument_attribute)
-          self.instruments << instrument
-            if !self.instruments.include?(instrument)
-            self.gig_instruments.build(:instrument => instrument)
-            end
+      instrument_attributes.values.each do |attr|
+      if attr[:name].present?
+        instrument = Instrument.find_or_create_by(name: attr[:name].titleize)
+        self.instruments << instrument
+        if !self.instruments.include?(instrument)
+          self.gig_instruments.build(:instrument => instrument)
           end
         end
       end
+    end
+
 
     def self.most_recent
         all.order('datetime asc')
     end
 
-    def authorized?
-      current_user.contractor
-    end
-end
+
